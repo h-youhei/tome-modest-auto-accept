3af460ecc6e5da631fd4651cf7b60bbc1aae5cfd
Hukumitu Youhei <hukumitu.youhei@gmail.com>
2021-03-21 16:28:29 +0900

    version update

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # tome4 Modest Auto-accept
 Base Game's Auto-accept target is too aggressive for me.
 This Addon provides restricted version of Auto-accept target.
-Auto-accept target if there is only one seen creature.
+Auto-accept target if there is only one seen creature or you are hovering a creature by mouse.
+
+Prevent some Talents from Auto-accept target.
+- Demented/Controlled-horrors/Decayed Bloated Horror
 
 This addon ignores Auto-accept target setting.
-
-Future update(Maybe):
-Auto-accept creature hovering by mouse.
-I can't figure out how to distinguish from last attacked creature. Help welcome.
+You can toggle Modest Auto-accept target by 'Toggle automatic accept target option' hotkey.
+Disable means Both of Modest Auto-accept and Base Game's Auto-accept are disabled regardless of 'Auto-accept target' setting.
--- init.lua
+++ init.lua
@@ -5,29 +5,30 @@ long_name = "Modest Auto-accept target"
 short_name = "modest-auto-accept" -- Determines the name of your addon's file.
 for_module = "tome"
 version = {1,7,2}
-addon_version = {1,0,1}
+addon_version = {1,1,0}
 weight = 99 -- The lower this value, the sooner your addon will load compared to other addons.
 author = {'hukumitu.youhei@gmail.com'}
 homepage = 'https://hkmtyh.com'
 description = [[Base Game's Auto-accept target is too aggressive for me.
 This Addon provides restricted version of Auto-accept target.
-Auto-accept target if there is only one seen creature.
+Auto-accept target if one of the followin is met
+- there is only one seen creature
+- you are hovering a creature by mouse
 
 Prevent some Talents from Auto-accept target.
 - Demented/Controlled-horrors/Decayed Bloated Horror
 
 This addon ignores Auto-accept target setting.
-
-Future update(Maybe):
-Auto-accept creature hovering by mouse.
-I can't figure out how to distinguish it from last attacked creature. Help welcome.
+You can toggle Modest Auto-accept target by 'Toggle automatic accept target option' hotkey.
+Disable means both of Modest Auto-accept and Base Game's Auto-accept are disabled regardless of 'Auto-accept target' setting.
 
 Github: https://github.com/h-youhei/tome-modest-auto-accept
 
 Weight: 99
 
 SuperLoad:
-- mod/class/Player.lua:getTarget()]] -- the [[ ]] things are like quote marks that can span multiple lines
+- mod/class/Player.lua:getTarget()
+- mod/class/Game.lua:setupCommands() to ]] -- the [[ ]] things are like quote marks that can span multiple lines
 tags = {'auto-accept'} -- tags MUST immediately follow description
 
 overload = false
